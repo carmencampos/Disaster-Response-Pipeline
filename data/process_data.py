917368b62d0b8d430069954b877912540c928d28
carmencampos <carcambor77@gmail.com>
2022-08-05 14:38:47 +0200

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -20,7 +20,7 @@ def load_data(messages, categories):
   return df, categories
 
 
-def processing_data(df):
+def clean_data(df):
   """
   create a dataframe of the 36 individual category columns
   select the first row of the categories dataframe
@@ -61,10 +61,39 @@ def save_data(df, db_name):
 
 
 def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
+    
+    
+"""
+def main():
   args = sys.argv[1:]
   df, categories = load_data(args[0], args[1])
   df = processing_data(df, categories)
   save_data(df, args[2])
-
-
-main()
+"""
